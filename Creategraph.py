785175c5de2b82802cbae877dce25c73c4f90779
Victor SIMONIN <victor.simonin@epita.fr>
2020-01-31 22:29:17 +0530

    Closely end

--- Creategraph.py
+++ Creategraph.py
@@ -2,7 +2,24 @@
 
 import graph
 import os
+import urllib
 
 def add_graph():
-    g = graph.Graph(50, False)
-    imglist = os.listdir("C:\Users\simon\Desktop\HACKATHON4.0\Images")
+    g= []
+
+    file = open("url.txt", "r")
+    url = file.readlines()
+
+    for j in range(len(url)):
+        g.append([])
+        for i in range(len(g)):
+            print(url[i])
+            s = input("Is there a link ?")
+            if s == "y":
+                s2 = input("Whats da link ??")
+                g[j].append((s2, i))
+
+
+
+
+add_graph()
