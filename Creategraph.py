4eb5508fd939b547347b3cca50f2b7b9f5683f00
Victor SIMONIN <victor.simonin@epita.fr>
2020-01-31 23:26:12 +0530

    OK

--- Creategraph.py
+++ Creategraph.py
@@ -7,19 +7,24 @@ import urllib
 def add_graph():
     g= []
 
-    file = open("url.txt", "r")
+    file = open("url1.txt", "r")
     url = file.readlines()
 
     for j in range(len(url)):
+
         g.append([])
+        print("new image we search link with...")
+        print(url[j])
         for i in range(len(g)):
-            print(url[i])
-            s = input("Is there a link ?")
-            if s == "y":
-                s2 = input("Whats da link ??")
-                g[j].append((s2, i))
+            if i != j:
+                print(url[i])
+                s = input("Is there a link ?")
+                if s == "y":
+                    s2 = input("Whats da link ??")
+                    g[j].append((s2, i))
 
 
 
 
 add_graph()
+
