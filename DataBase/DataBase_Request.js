8a99e924ef171425351218108c9bff551de933bc
jeannemorin <jeanne.morin@epita.fr>
2020-02-02 02:25:29 +0530

    javascript aled

--- DataBase/DataBase_Request.js
+++ DataBase/DataBase_Request.js
@@ -1,32 +1,67 @@
 {
-function made_request(word){
+function make_request(word){
     var unirest = require("unirest");
     var url = "https://api.wordassociations.net/associations/v1.0/json/search?apikey=4b8acd47-07b3-4d38-ab4d-2480c73669e9"
     var a = url.concat("&type=stimulus&indent=yes&pos=noun&limit=50&lang=en&text=");
     querystring = {"apikey":"4b8acd47-07b3-4d38-ab4d-2480c73669e9","type":"stimulus","indent":"yes","pos":"noun","limit":"50","lang":"en","text":word};
     unirest.get(a.concat(word),params=querystring).header("apikey","4b8acd47-07b3-4d38-ab4d-2480c73669e9").end(function (result) {
-        console.log(result.raw_body);  
+           
     
+    var l = new Array();
+    var res = l.concat(parser_json(result.raw_body));
     
-    const L = JSON.parse(result.raw_body);
-            
+    return res;
     });
+}
+}     
+    
+function parser_json(path){
+    var a=0;
+    var l = new Array();
+    for(i=0; i<path.length;i++){
+        
+        if(path[i] == 'i' && path[i+1] == 't' && path[i+2] == 'e' && path[i+3] == 'm' && path[i+4] != 's'){
+            i += 8;
+            var s = " ";
+            
+            while(path[i+1] != ','){
+                
+                s = s.concat(path[i]);
+                i += 1;
+                
+            }
+            l.push(s);
+            a+=1;
+        }
+    }
 
+    return (l,a);
 }
 
-made_request("bird");
+
 
 function linked_list(w1,w2){
-    var L = []
-    var K = []
-    var out = []
-    for(i=0; i<length(L); i++){
-        for(j=0; j<length(K);j++){
+    var a = new Array();
+    var L = a.concat(make_request(w1));
+   
+    var b = new Array();
+    var K = b.concat(make_request(w2));
+
+    var out = [];
+
+    for(i=0; i<L.length; i++){
+        for(j=0; j<K.length;j++){
             if(L[i]==K[j]){
                 out.push(L[i]);
             }
         }
     }
+    for(i=0;i<out.length;i++){
+        console.log(out[i]);
+    }
+    return out;
 }
 
-}   
+linked_list("bread","jam");
+
+
