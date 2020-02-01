3370b75ccba7e6a5aade6ec937bf6475a2fca876
jeannemorin <jeanne.morin@epita.fr>
2020-02-01 14:32:58 +0530

    access to data base

--- DataBase/Python_Request.py
+++ DataBase/Python_Request.py
@@ -6,13 +6,54 @@ Created on Sat Feb  1 10:06:32 2020
 """
 
 import requests
+import json
 
-def made_request():
+
+def made_request(word):
 
     url = "https://api.wordassociations.net/associations/v1.0/json/search?"
 
-    querystring = {"apikey":"4b8acd47-07b3-4d38-ab4d-2480c73669e9","type":"stimulus","indent":"yes","pos":"noun","limit":"50","lang":"en","text":"apple"}
+    querystring = {"apikey":"4b8acd47-07b3-4d38-ab4d-2480c73669e9","type":"stimulus","indent":"yes","pos":"noun","limit":"50","lang":"en","text":word}
 
     r = requests.get(url,params=querystring)
 
-    print(r.text)
+    L = json.loads(r.text)
+    K= L['response']
+    K = K[0]
+    K = K['items']
+    
+    final = []
+    for i in K:
+        final.append(i['item'])
+    
+    return final
+
+
+
+def main(w1,w2):
+    
+    L = made_request(w1)
+    K = made_request(w2)
+    
+    for i in L:
+        for j in K:
+            if(i==j):
+                return i
+    
+    return 0
+
+def main_list(w1,w2):
+    
+    L = made_request(w1)
+    K = made_request(w2)
+    out = []
+    
+    for i in L:
+        for j in K:
+            if(i==j):
+                out.append(i)            
+    
+    return out
+    
+    
+
