ff0178eadcd67a0868b10e7065f5cc22dd10651f
Alexandra Petit <alexandra.petit@epita.fr>
2020-02-01 09:10:27 +0530

    alex
    Signed-off-by: Alexandra Petit <alexandra.petit@epita.fr>

---
+++ Test/popup4.js
@@ -0,0 +1,11 @@
+window.onload = function (name) {
+
+    var shopping = document.getElementById("shopping");
+    shopping.addEventListener("click", Wshopping);
+    function Wshopping()
+    {
+        window.open("index.html","Shopping");
+    }
+
+
+}
